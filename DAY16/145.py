1bfab8e490f60abcc42b4375a4ff0ce7d46c8bc8
ufo607 <ufo607.off@gmail.com>
2021-09-30 16:26:17 +0500

    Finish Day 16

---
+++ DAY16/145.py
@@ -0,0 +1,17 @@
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape('turtle')
+# timmy.bgcolor('red')
+# timmy.dot(100)
+
+# myscreen = Screen()
+# myscreen.exitonclick()
+
+
+# --------------------------------
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column('name', ['roma', 'ufo'])
+print(table)
